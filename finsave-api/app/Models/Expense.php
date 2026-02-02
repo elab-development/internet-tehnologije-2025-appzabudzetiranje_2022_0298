c7b6ad7715d5534c4efaa660660d06f4fb4c1994
Lencili <magdalenabojanic03@gmail.com>
2026-02-02 12:53:00 +0100

    Ažuriran Expense model

--- finsave-api/app/Models/Expense.php
+++ finsave-api/app/Models/Expense.php
@@ -2,9 +2,36 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Expense extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        "payer_id",
+        "category_id",
+        "description",
+        "amount",
+        "paid_at",
+    ]; // 
+    protected $casts= [
+        "amount" => 'decimal>z', 
+        "paid_at" => 'datetime',
+        ]; //kastovanje automatski
+
+        public function payer()
+        {
+            return $this->belongsTo(User::class, 'payer_id');
+        }
+            public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function participants()
+    {
+        return $this->hasMany(ExpenseParticipant::class);
+    }
 }
